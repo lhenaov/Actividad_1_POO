31c0b4eed344334c3a58cb02a1a3072a0d43256a
lhenaov <lhenaov@unal.edu.co>
2026-02-10 17:47:49 -0500

    Create Edades.java

---
+++ Ejercicio_4/Edades.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.actividad1;
+
+/**
+ *
+ * @author 57316
+ */
+public class Edades {
+    public static double calcular_edalber(double edjuan) {
+    double edalber= 2*edjuan/3;
+    return edalber;
+    }
+    
+    public static double calcular_edana(double edjuan) {
+    double edana=4*edjuan/3;
+    return edana;
+    }
+    
+    public static double calcular_edmama (double edjuan, double edalber, double edana) {
+    double  edmama=edjuan + edalber + edana;
+    return edmama;
+    }
+        
+       }
