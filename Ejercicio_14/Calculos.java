26ad7cd0e9d653294d5590d9874d06625064ffbe
lhenaov <lhenaov@unal.edu.co>
2026-02-11 20:52:41 -0500

    Create Calculos.java

---
+++ Ejercicio_14/Calculos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicio14;
+
+/**
+ *
+ * @author 57316
+ */
+public class Calculos {
+      public static double calcular_cuadrado(double numero) {
+        return Math.pow(numero, 2);
+    }
+
+    public static double calcular_cubo(double numero) {
+        return Math.pow(numero, 3);
+    }
+
+}
