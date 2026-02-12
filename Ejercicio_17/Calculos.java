17c3a32020b39d9af0264fe6136ef81c6195d836
lhenaov <lhenaov@unal.edu.co>
2026-02-11 21:09:02 -0500

    Create Calculos.java

---
+++ Ejercicio_17/Calculos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicio17;
+
+/**
+ *
+ * @author 57316
+ */
+public class Calculos {
+      public static double calcular_longitud_circunferencia(double radio) {
+        return 2* Math.PI *radio;
+    }
+
+    public static double calcular_area_circulo(double radio) {
+        return  Math.PI *Math.pow(radio,2);
+    }
+
+}
