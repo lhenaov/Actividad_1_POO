84db5550d45d054ec353bb6a633a62077df86bda
lhenaov <lhenaov@unal.edu.co>
2026-02-11 20:52:11 -0500

    Create main.java

---
+++ Ejercicio_14/main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio14;
+
+/**
+ *
+ * @author 57316
+ */
+public class Ejercicio14 {
+
+    public static void main(String[] args) {
+        double numero = 4;
+
+        double cuadrado = Calculos.calcular_cuadrado(numero);
+        double cubo = Calculos.calcular_cubo(numero);
+
+        System.out.println("Cuadrado " + cuadrado);
+        System.out.println("Cubo " + cubo);
+    }
+}
